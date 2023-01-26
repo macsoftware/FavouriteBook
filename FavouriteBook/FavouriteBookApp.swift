254ae78f949d116a445f304d05b1250c70d4bdf7
macsoftware <87543117+macsoftware@users.noreply.github.com>
2023-01-26 15:56:14 +0000

    Initial Commit

---
+++ FavouriteBook/FavouriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteBookApp.swift
+//  FavouriteBook
+//
+//  Created by Ian MacKinnon on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
